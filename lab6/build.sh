630d2491aa317a5f8b956a8a2e64de1a18442238
Dima Voloshyn <162379113+ZACRAN1C@users.noreply.github.com>
2026-06-13 07:42:37 +0300

    Delete lab6 directory

--- lab6/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "=== Збірка проєкту за допомогою CMake ==="
-mkdir -p build
-cd build || exit
-cmake ..
-make
--- lab6/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Запуск програми..."
-cd build
-./OpenCV_Lab6
